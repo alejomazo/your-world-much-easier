9ecd1d7e3149f4ffb266a7b6b7d90c9b83ea3bc4
alejomazo <146904695+alejomazo@users.noreply.github.com>
2023-10-03 21:38:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const menu= document.querySelector("#menu");
+const hamburguer= document.querySelector("#hamburguer");
+
+
+hamburguer.addEventListener('click',()=>{
+    if(menu.style.display =='none'){
+        menu.style.display ='flex';
+    }else{
+        menu.style.display ='none';
+    }
+
+})
